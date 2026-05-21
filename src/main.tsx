23442e35bc7570d5c616e7da845f56c9f7ccb496
Ryan Wilson <syndicated.life@gmail.com>
2026-05-21 01:20:34 -0700

    fix: remove manual publishableKey prop from ClerkProvider (reads from env automatically)

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,9 @@ import { ClerkProvider } from '@clerk/react';
 import './index.css';
 import App from './App.tsx';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
-if (!PUBLISHABLE_KEY) throw new Error('VITE_CLERK_PUBLISHABLE_KEY is not set');
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider afterSignOutUrl="/">
       <BrowserRouter>
         <App />
       </BrowserRouter>
